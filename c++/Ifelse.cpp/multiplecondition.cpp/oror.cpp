ee1b80523b70f821f5ef2776120b8679d148e626
2025b01010193 <rimjhim.25b01010193@abes.ac.in>
2026-08-23 00:03:17 +0530

    If-else introduction

---
+++ c++/Ifelse.cpp/multiplecondition.cpp/oror.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cout<<"Enter a number : ";
+    cin>>x;
+    if((x%5==0)||(x%3==0)){
+        cout<<"x is divisible by 3 or 5";
+    }
+    else{
+        cout<<"not divisible";
+    }
+}
