ec11a15eb9c48563ad858c6ba662aa27b1a8347d
2025b01010193 <rimjhim.25b01010193@abes.ac.in>
2026-08-25 12:00:20 +0530

    Assignment operator

---
+++ c++/Assingment.cpp/Boolen.cpp/bool.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+int main(){
+    bool x = true;
+    cout<<x;
+}
---
+++ c++/Assingment.cpp/chardatatype.cpp/chartoascii.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char ch = 'b';
+    int ascii = (int)ch;
+    cout<<ascii;
+}
